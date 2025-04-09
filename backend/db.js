60559801d588614e83a311491803031ee9285925
keval2292 <ramanikeval194@gmail.com>
2025-04-09 23:24:59 +0530

    chnage the mongo url

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connecToMongo = async () => {
     try {
-        await mongoose.connect('mongodb+srv://kevalnramanikr:x1qlvVdh7dcXc6Np@cluster0.adhmz.mongodb.net/quicknote?retryWrites=true&w=majority&appName=Cluster0'); // Removed deprecated options
+        await mongoose.connect('mongodb+srv://kevalnramanikr:ReCH9dOEEqJh4mah@cluster0.adhmz.mongodb.net/quicknote?retryWrites=true&w=majority&appName=Cluster0'); // Removed deprecated options
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Connection error:', err);
